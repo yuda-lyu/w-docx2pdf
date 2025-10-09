86a0e6e7718627e0da19743d72200c999e9dbef1
yuda-lyu <firsemisphere@gmail.com>
2025-10-09 13:05:08 +0800

    modify: add supp. LibreOffice

--- src/WDocx2pdf.mjs
+++ src/WDocx2pdf.mjs
@@ -8,9 +8,10 @@ import str2b64 from 'wsemi/src/str2b64.mjs'
 import execProcess from 'wsemi/src/execProcess.mjs'
 import fsIsFile from 'wsemi/src/fsIsFile.mjs'
 import fsIsFolder from 'wsemi/src/fsIsFolder.mjs'
-import fsCreateFolder from 'wsemi/src/fsCreateFolder.mjs'
+import fsCopyFile from 'wsemi/src/fsCopyFile.mjs'
 import fsDeleteFile from 'wsemi/src/fsDeleteFile.mjs'
 import fsRenameFile from 'wsemi/src/fsRenameFile.mjs'
+import fsCreateFolder from 'wsemi/src/fsCreateFolder.mjs'
 
 
 let fdSrv = path.resolve()
@@ -28,6 +29,8 @@ function isWindows() {
  * @param {String} fpOut 輸入轉出pdf檔位置字串
  * @param {Object} [opt={}] 輸入設定物件，預設{}
  * @param {String} [opt.fdTemp='./_convertTempPdf'] 輸入臨時檔位置字串，預設'./_convertTempPdf'
+ * @param {String} [opt.mode='MicrosoftOffice'] 輸入執行模式字串，可使用'MicrosoftOffice'與'LibreOffice'，若要使用皆須安裝軟體，預設'MicrosoftOffice'
+ * @param {String} [opt.fpExeLibreOffice='C:\\Program Files\\LibreOffice\\program\\soffice.exe'] 輸入若mode='LibreOffice'，須提供LibreOffice執行檔soffice.exe位置字串，預設'C:\\Program Files\\LibreOffice\\program\\soffice.exe'
  * @returns {Promise} 回傳Promise，resolve回傳成功訊息，reject回傳錯誤訊息
  * @example
  *
@@ -67,6 +70,18 @@ async function WDocx2pdf(fpIn, fpOut, opt = {}) {
         return Promise.reject(`fpIn[${fpIn}] does not exist`)
     }
 
+    //mode
+    let mode = get(opt, 'mode', '')
+    if (mode !== 'MicrosoftOffice' && mode !== 'LibreOffice') {
+        mode = 'MicrosoftOffice'
+    }
+
+    //fpExeLibreOffice
+    let fpExeLibreOffice = get(opt, 'fpExeLibreOffice', '')
+    if (!fsIsFile(fpExeLibreOffice)) {
+        fpExeLibreOffice = 'C:\\Program Files\\LibreOffice\\program\\soffice.exe'
+    }
+
     //fdTemp
     let fdTemp = get(opt, 'fdTemp')
     if (!isestr(fdTemp)) {
@@ -76,66 +91,109 @@ async function WDocx2pdf(fpIn, fpOut, opt = {}) {
         fsCreateFolder(fdTemp)
     }
 
-    //fnExe
-    let fnExe = `cv.exe`
-
-    //fdExe
-    let fdExe = ''
-    if (true) {
-        let fdExeSrc = `${fdSrv}/src/`
-        let fdExeNM = `${fdSrv}/node_modules/w-docx2pdf/src/`
-        if (fsIsFile(`${fdExeSrc}${fnExe}`)) {
-            fdExe = fdExeSrc
-        }
-        else if (fsIsFile(`${fdExeNM}${fnExe}`)) {
-            fdExe = fdExeNM
-        }
-        else {
-            return Promise.reject('can not find folder for docx2pdf')
-        }
-    }
-    // console.log('fdExe', fdExe)
-
-    //prog
-    let prog = `${fdExe}${fnExe}`
-    // console.log('prog', prog)
+    //轉絕對路徑
+    fpIn = path.resolve(fpIn)
 
     //id
     let id = `${now2strp()}_${genID(6)}`
+
+    //fnInTemp
+    let fnInTemp = `docx2pdf_${id}.docx`
+
+    //fpInTemp
+    let fpInTemp = `${fdTemp}/${fnInTemp}`
+    fpInTemp = path.resolve(fpInTemp)
+
+    //fsCopyFile
+    fsCopyFile(fpIn, fpInTemp)
 
     //fnOutTemp
     let fnOutTemp = `docx2pdf_${id}.pdf`
 
     //fpOutTemp
     let fpOutTemp = `${fdTemp}/${fnOutTemp}`
-
-    //轉絕對路徑
-    fpIn = path.resolve(fpIn)
     fpOutTemp = path.resolve(fpOutTemp)
 
-    //inp
-    let inp = {
-        fpIn,
-        fpOut: fpOutTemp,
-        opt: {},
+    if (mode === 'MicrosoftOffice') {
+
+        //fnExe
+        let fnExe = `cv.exe`
+
+        //fdExe
+        let fdExe = ''
+        if (true) {
+            let fdExeSrc = `${fdSrv}/src/`
+            let fdExeNM = `${fdSrv}/node_modules/w-docx2pdf/src/`
+            if (fsIsFile(`${fdExeSrc}${fnExe}`)) {
+                fdExe = fdExeSrc
+            }
+            else if (fsIsFile(`${fdExeNM}${fnExe}`)) {
+                fdExe = fdExeNM
+            }
+            else {
+                return Promise.reject('can not find folder for docx2pdf')
+            }
+        }
+        // console.log('fdExe', fdExe)
+
+        //prog
+        let prog = `${fdExe}${fnExe}`
+        // console.log('prog', prog)
+
+        //inp
+        let inp = {
+            fpIn: fpInTemp,
+            fpOut: fpOutTemp,
+            opt: {},
+        }
+        // console.log('inp', inp)
+
+        //input to b64
+        let cInput = JSON.stringify(inp)
+        let b64Input = str2b64(cInput)
+        // console.log('b64Input', b64Input)
+
+        //execProcess
+        await execProcess(prog, b64Input)
+            .catch((err) => {
+                console.log('execProcess catch', err)
+                errTemp = err.toString()
+            })
+
+        //check
+        if (errTemp) {
+            return Promise.reject(errTemp)
+        }
+
     }
-    // console.log('inp', inp)
+    else if (mode === 'LibreOffice') {
 
-    //input to b64
-    let cInput = JSON.stringify(inp)
-    let b64Input = str2b64(cInput)
-    // console.log('b64Input', b64Input)
+        //prog
+        let prog = fpExeLibreOffice
+        // console.log('prog', prog)
 
-    //execProcess
-    await execProcess(prog, b64Input)
-        .catch((err) => {
-            console.log('execProcess catch', err)
-            errTemp = err.toString()
-        })
+        //args
+        let args = [
+            '--headless',
+            '--norestore',
+            '--invisible',
+            '--convert-to', 'pdf:writer_pdf_Export',
+            '--outdir', fdTemp,
+            fpInTemp,
+        ]
 
-    //check
-    if (errTemp) {
-        return Promise.reject(errTemp)
+        //execProcess
+        await execProcess(prog, args)
+            .catch((err) => {
+                console.log('execProcess catch', err)
+                errTemp = err.toString()
+            })
+
+        //check
+        if (errTemp) {
+            return Promise.reject(errTemp)
+        }
+
     }
 
     //check
@@ -143,14 +201,18 @@ async function WDocx2pdf(fpIn, fpOut, opt = {}) {
         return Promise.reject(`can not convert`)
     }
 
-    //fsRenameFile
+    //fsCopyFile
     fsDeleteFile(fpOut)
-    let r = fsRenameFile(fpOutTemp, fpOut)
+    let r = fsCopyFile(fpOutTemp, fpOut)
 
     //check
     if (r.error) {
         return Promise.reject(r.error)
     }
+
+    //fsDeleteFile
+    fsDeleteFile(fpInTemp)
+    fsDeleteFile(fpOutTemp)
 
     return 'ok'
 }
--- srcPython/run.bat
+++ srcPython/run.bat
@@ -1,4 +1,4 @@
-C:\Users\SemiZenBook15\AppData\Local\Programs\Python\Python311\python.exe cv.py eyJmcEluIjogInp0bXAuZG9jeCIsICJmcE91dCI6ICJ6dG1wLnBkZiIsICJvcHQiOiB7fX0=
+python cv.py eyJmcEluIjogInp0bXAuZG9jeCIsICJmcE91dCI6ICJ6dG1wLnBkZiIsICJvcHQiOiB7fX0=
 pause
 
 #測試版使用

75949618086aec69a89c677d0552c6f04e3b7440
yuda-lyu <firsemisphere@gmail.com>
2026-07-22 19:04:40 +0800

    modify: update pks

--- srcPython/cv.py
+++ srcPython/cv.py
@@ -48,7 +48,7 @@ def str2b64(v):
 def b642str(v):
     #base64字串轉字串
     import base64
-    return base64.b64decode(v)
+    return base64.b64decode(v).decode('utf-8')
 
 
 def readText(fn):
@@ -108,7 +108,7 @@ def core(b64):
         state='success'
     except:
         err=getError()
-        state='error: [core]'+err["message"]
+        state='error: '+str(err["message"])
 
     return state
 
@@ -130,7 +130,7 @@ def run():
         
     else:
         #print(sys.argv)
-        state='error: [run]invalid length of argv'
+        state='error: invalid length of argv'
     
     #print & flush
     print(state)
